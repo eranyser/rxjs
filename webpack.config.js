8dac9957e279c9638c6535795cee11d9c45dc02d
eranyser <eran.weiser@gmail.com>
2017-09-23 01:19:54 +0300

    updating some elements as described in release-03 of readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,45 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'},
-          {loader: 'sass-loader'}
-        ]
-      },
-      {
-          test: /\.ts$/,
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
           use: [
-              {loader: 'awesome-typescript-loader'}
+              'style-loader',
+              'css-loader'
           ]
-      },
-    ]
-  },
-  plugins: [new HtmlWebpackPlugin({
-      template: "./src/index.html"
-  })]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              {loader: 'style-loader'},
+              {loader: 'css-loader'},
+              {loader: 'sass-loader'}
+          ]
+        },
+        {
+            test: /\.ts$/,
+            use: [
+                {loader: 'awesome-typescript-loader'}
+            ]
+        },
+      ]
+    },
+
+    resolve: {
+        /*
+         * An array of extensions that should be used to resolve modules.
+         * you can now require('file') instead of require('file.js')
+         */
+        extensions: ['*', '.ts', '.js', '.json'],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/index.html"
+    })]
 };
