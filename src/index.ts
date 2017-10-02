9b6d3000f3b5a51e64398847f39b7b7fa92aafa2
eranyser <eran.weiser@gmail.com>
2017-10-02 22:33:30 +0300

    adding scss red bakcground

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 //import "core-js";
-
+import './scss/main.scss';
 //import { Observable } from '../node_modules/rxjs/Observable';
-import { Observable } from 'rxjs/Observable';
+//import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import { Observable } from "rxjs/Observable";
 
 let numbers = [5, 10, 15];
 
 let source = Observable.from(numbers);
+
 
 class MyObserver {
     next(value) {
